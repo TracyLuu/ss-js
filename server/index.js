1c60ecab45bd5a76d3095c0e87655e2a9f3e45e6
Tracy Luu <candieparade@gmail.com>
2020-03-10 22:58:04 +0000

    added the 404 message in the product does not exit

--- server/index.js
+++ server/index.js
@@ -38,16 +38,23 @@ app.get('/api/products/:productId', (req, res, next) => {
   `;
 
   const params = [productId];
-  if (productId.length < productId || productId <= 0) {
+  if (productId > 6 || productId <= 0) {
     return next(new ClientError('Invalid product', 400));
-  } else {
+  } else if (productId === '') {
     res.status(500).json({
-      error: 'an unexpected error occurred'
+      error: 'An unexpected error occurred'
     });
   }
   db.query(sql, params)
-    .then(result => res.json(result.rows[0]))
-    .catch(err => next(err));
+    .then(result => {
+      return res.json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(404).json({
+        error: 'There may be an error querying the database.'
+      });
+    });
 });
 
 app.use('/api', (req, res, next) => {
