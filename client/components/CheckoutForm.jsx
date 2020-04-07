7fd25262657d479f0468840edac4c10449f821a3
Tracy Luu <candieparade@gmail.com>
2020-04-07 09:10:05 +0000

    got the name credit card and shipping address to look ok

--- client/components/CheckoutForm.jsx
+++ client/components/CheckoutForm.jsx
@@ -5,24 +5,32 @@ export default class CheckoutForm extends React.Component {
   render() {
     return (
       <form className="container">
-        <label>
-          <div>
+        <label className="container">
+          <div className="row">
             Name:
           </div>
-          <div>
+          <div className="row">
             <input type="text" name="name">
             </input>
           </div>
         </label>
-        <label className="row">
-          Credit Card:
-          <input type="text" name="credit-card">
-          </input>
+        <label className="container">
+          <div className="row">
+            Credit Card:
+          </div>
+          <div className="row">
+            <input type="text" name="credit-card">
+            </input>
+          </div>
         </label>
-        <label className="row">
-          Shipping Address:
-          <input type="text" name="shipping-address">
-          </input>
+        <label className="container">
+          <div className="row">
+            Shipping Address:
+          </div>
+          <div className="row">
+            <input type="text" name="shipping-address">
+            </input>
+          </div>
         </label>
         <div>
           <button type="submit" className="btn btn-primary">Place Order</button>
