aed59eee76f84e300b3fafcf83d76063ff4366e9
Tracy Luu <candieparade@gmail.com>
2020-04-12 11:06:56 +0000

    trimmed off more bootstrap things i didnt need in the header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,7 +6,10 @@ export default class Header extends React.Component {
     return (
       <div className="bg-dark m-0 d-flex">
         <div className="row m-0 p-2">
-          <div className="text-white title"> <i className="fas fa-dollar-sign left"></i> Wicked Sales</div>
+          <div className="text-white">
+            <i className="fas fa-dollar-sign pr-2">
+            </i>Wicked Sales
+          </div>
           <div className="text-white logo">
           </div>
         </div>
@@ -14,8 +17,8 @@ export default class Header extends React.Component {
           <span className="d-block p-2 text-white">
             <div className="text-white item" onClick={(name, params) => this.props.setView('cart',
               {})}>
-              {this.props.cart.length} Items { }
-              <i className="text-white fas fa-shopping-cart">
+              {this.props.cart.length} Items
+              <i className="text-white fas fa-shopping-cart pl-2">
               </i>
             </div>
           </span>
