d8d36fe21e3a98c1138439ae76595380bf682e57
Tracy Luu <candieparade@gmail.com>
2020-04-13 04:19:30 +0000

    made the product lists responsive

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -46,7 +46,6 @@ export default class CheckoutForm extends React.Component {
       this.props.setView('catalog', {});
       this.props.cart([]);
     }
-
   }
 
   render() {
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,7 +6,7 @@ export default class ProductListItem extends React.Component {
     if (this.props.view.name === 'catalog') {
       return (
         this.props.products.map((product, index) =>
-          <div className="col-4" key={index}>
+          <div className="media p-2 centered" key={index}>
             <div className="alignment card mt-2" onClick={(name, params) => this.props.setView(
               'details',
               { productId: product.productId })}>
