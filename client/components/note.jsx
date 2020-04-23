afcc64f4f82ce04c7a09c5db3a603220f6d8bfcc
Tracy Luu <candieparade@gmail.com>
2020-04-23 00:50:13 +0000

    removed commented out code

--- client/components/note.jsx
+++ client/components/note.jsx
@@ -30,18 +30,5 @@ export default class Note extends React.Component {
         <div></div>
       );
     }
-
-    // <div className="container pt-5">
-    //   <div className="text-center pt-5">
-    //     <h4 className="p-2">
-    //       Please note that this shop is for demonstration purposes only.
-    //       Please do not provide real information when checking out.
-    //       Thank you and enjoy browsing!
-    //     </h4>
-    //     <div className="p-2">
-    //       <button type="button" className="btn btn-info" data-dismiss="modal" onClick={this.props.toCatalog}>Understood</button>
-    //     </div>
-    //   </div>
-    // </div>
   }
 }
