959adbdd96ddf14963653c7ee3afa74f05047907
Tracy Luu <candieparade@gmail.com>
2020-04-28 00:35:28 +0000

    got the modal to show up

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -4,53 +4,57 @@ export default class CartSummaryItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: true
+      visible: false,
+      modal: false
     };
     this.deleteCartById = this.deleteCartById.bind(this);
+    this.modalCancel = this.modalCancel.bind(this);
+    this.modalDelete = this.modalDelete.bind(this);
   }
 
-  deleteCartById(cartItem) {
+  deleteCartById() {
+    this.setState({ modal: true });
+  }
 
-    // const cartItemId = cartItem.cartItemId;
-    // this.props.deleteCart(cartItemId);
+  modalCancel() {
+    this.setState({ visible: false });
+  }
 
-    // if (this.state.visible === true) {
-    //   return (
-    //     <>
-    //       <DeleteCartModal />
-    //     </>
-    //   );
-    // }
+  modalDelete(cartItem) {
+    this.setState({ visible: false });
+    const cartItemId = cartItem.cartItemId;
+    this.props.deleteCart(cartItemId);
+
   }
 
   createCartItems() {
-    if (this.state.visible === false) {
-      this.setState({ visible: true });
-    }
     return (this.props.cart.map((cartItem, index) =>
 
       <div className="pl-2 pr-2" key={index}>
         <div className="card mb-3 p-3 cart">
           <div className="row pl-3">
-            <div className="exampleModal justify-content-center p-3">
-              <div className="modal-title text-center">
-                Are you sure?
-              </div>
-              <div className="d-flex justify-content-center">
-                <div className="p-2">
-                  <button className="btn btn-secondary p-2">
-                    Cancel
-                  </button>
-                </div>
-                <div className="p-2">
-                  <button className="btn btn-danger p-2">
-                    Delete
-                  </button>
-                </div>
 
+            <div className="exampleModal justify-content-center p-3 hidden">
+              <div className="modal-background">
+                <div className="modal-title text-center">
+                  Are you sure?
+                </div>
+                <div className="d-flex justify-content-center">
+                  <div className="p-2">
+                    <button className="btn btn-secondary p-2">
+                      Cancel
+                    </button>
+                  </div>
+                  <div className="p-2">
+                    <button className="btn btn-danger p-2">
+                      Delete
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-            <i className="fas fa-times" onClick={ () => this.deleteCartById(cartItem)}></i>
+
+            <i className="fas fa-times" onClick={ this.deleteCartById}></i>
           </div>
           <div className="centered">
             <img className="smallImg card-img-top col-12 col-md-4" src={cartItem.image}></img>
@@ -67,6 +71,27 @@ export default class CartSummaryItem extends React.Component {
   }
 
   render() {
+    if (this.props.modal === true) {
+      return (
+        <div className="exampleModal justify-content-center p-3">
+          <div className="modal-title text-center">
+            Are you sure?
+          </div>
+          <div className="d-flex justify-content-center">
+            <div className="p-2">
+              <button className="btn btn-secondary p-2">
+                Cancel
+              </button>
+            </div>
+            <div className="p-2">
+              <button className="btn btn-danger p-2">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
     if (this.props.cart.length === 0) {
       return (
         <div className="container p-5 justify-content-center">
